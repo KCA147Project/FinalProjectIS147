a7f2705e351b5e22029fd330e6156a32c38c7c0e
chris <catkins2@umbc.edu>
2022-05-15 13:17:24 -0400

    Reorganizing state tax and itemname

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,78 +1,20 @@
+package com.company;
+
 import java.text.*;
 import java.time.*;
 import java.time.format.*;
 import java.util.*;
 
-
-
 public class Main {
 
-
-
     public static void main(String[] args) {
-
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy | HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-
-        String line = new String(new char[48]).replace('\0', '_');
-        String line2 = new String(new char[48]).replace('\0', '*');
-
-
-
-
-//_____________________________________________________________________________________________________________________
-        //EMPLOYEE ID CLASS
-
-        String Andrew = "326099 Stine A";
-        String Chris = "175816 Atkinson C";
-        String Kevin = "223296 Shields K";
-
-
-//_____________________________________________________________________________________________________________________
-        //ARRAY HEADER CLASS
-
-
-        ArrayList<String> tableHeader = new ArrayList();
-        tableHeader.add("ITEM"); //0
-        tableHeader.add("QTY"); //1
-        tableHeader.add("PRICE"); //2
-        tableHeader.add("TOTAL"); //3
 
 
 //_____________________________________________________________________________________________________________________
         //ARRAY ITEM NAME CLASS
-
-        ArrayList<String> itemList = new ArrayList();
-        itemList.add("EGO 56-volt Select Cut"); //0
-        itemList.add("iPhone 13 Pro Max (1TB)"); //1
-        itemList.add("T-Rex Skull"); //2
-        itemList.add("Playstation V"); //3
-        itemList.add("TAG Heuer Monaco Calibre 11"); //4
-        itemList.add ("MacBook Pro 16-inch"); //5
-        itemList.add("Airpods Max"); //6
-        itemList.add("Samsung 65-inch 4K Smart TV"); //7
-        itemList.add("Milwaukee Brushless Combo"); //8
-        itemList.add("iPad Pro 12.9-inch (2TB)"); //9
-        itemList.add("NexiGo Webcam"); //10
-        itemList.add("Blurams Security Camera"); //11
-        itemList.add("Occer 12x25 Compact Binoculars"); //12
-        itemList.add("TOPS Opening Day Baseball Value Pack"); //13
-        itemList.add("Retro Typewriter Keyboard 7 Keys"); //14
-        itemList.add("Hydro Flask"); //15
-        itemList.add("Auking Mini Projector"); //16
-        itemList.add("Nike Tech Jacket"); //17
-        itemList.add("Yeezy Boost 350v2 Bred"); //18
-        itemList.add("Elden Ring"); //19
-        itemList.add("Comfort Carry Portable Locker Pack, 42L"); //20
-        itemList.add("AquaSonic Black Series"); //21
-        itemList.add("Crest Pro-Health"); //22
-        itemList.add("Blue Buffalo Life Protection"); //23
-        itemList.add("WEWLINE 17-inch Toolbox"); //24
-        itemList.add("BIC Xtra-Smooth Mechanical Pencils"); //25
-        itemList.add("3 Pack Chargers (AppleMFi Certified)"); //26
-        itemList.add("Amazon Basics Small Digital Alarm Clock"); //27
-        itemList.add("Tesla Model 3 Performance"); //28
-        itemList.add("Meepo V3 Electric Skateboard"); //29
+        itemName name = new itemName();
+        name.displayItemMenu();
+        int item_id = name.enterItemId();
 
 //_____________________________________________________________________________________________________________________
         //ARRAY ITEM PRICE CLASS
@@ -101,11 +43,9 @@ public class Main {
         //NAME (QUESTION)
         //Scanner input = new Scanner(System.in);
 
-       //System.out.println("\nWhat state are you from? (Abbreviate e.g. CA): ");
-        //String value = input.nextLine();
-
         StateTaxes taxObject = new StateTaxes();
-        System.out.println(taxObject.StateTaxes());
+        String state = taxObject.getLocation();
+        double taxRate = taxObject.getTax(location);
 
 
         //EMPLOYEE ID
@@ -211,12 +151,13 @@ public class Main {
         System.out.println(itemList.get(0) + "       " + fmt1.format(itemprice[0]));
         System.out.println(itemList.get(18) + "       " + fmt1.format(itemprice[18]));
 
-
-
-
-
-
-
     }
 }
 
+/*
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy | HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        String line = new String(new char[48]).replace('\0', '_');
+        String line2 = new String(new char[48]).replace('\0', '*');
+ */
--- src/com/company/itemName.java
+++ src/com/company/itemName.java
@@ -1,106 +1,58 @@
+package com.company;
+
 import java.util.*;
 
 public class itemName {
+    public void displayItemMenu() {
+        System.out.println("Enter the item numbers you would like to purchase: ");
+        System.out.println("----------------------------------------");
 
-    Scanner name = new Scanner(System.in);
+        System.out.println("ELECTRONICS:");
+        System.out.println("#0 - Electric Lawn Mower $799.00");
+        System.out.println("#1 - iPhone $1,599.00");
+        System.out.println("#3 - Game Console $499.00");
+        System.out.println("#4 - Automatic Watch $5,715.00");
+        System.out.println("#5 - Laptop $6,099.00");
+        System.out.println("#6 - AirPods Max $549.00");
+        System.out.println("#7 - TV $529.00");
+        System.out.println("#9 - Tablet $2,399.00");
+        System.out.println("#10 - Beats Headphones $129.95");
+        System.out.println("#11 - Security Camera $40.98");
+        System.out.println("#14 - Electronic Retro Type Writer $138.99");
+        System.out.println("#16 - Projector $89.99");
+        System.out.println("#19 - Video Game $59.99");
+        System.out.println("#26 - iPhone Charger $21.99");
+        System.out.println("#27 - Alarm Clock $10.34");
+        System.out.println("#28 - Tesla Model 3 $76,990.00");
+        System.out.println("#29 - Electronic Skateboard $599.00");
 
-    String name;
+        System.out.println("------------------------------");
+        System.out.println("CLOTHING:");
+        System.out.println("#17 - Jacket $121.63");
+        System.out.println("#18 - Yeezy 350 Sneakers $412.00");
+        System.out.println("#20 - Back Pack $99.00");
 
-    {
+        System.out.println("------------------------------");
+        System.out.println("HOUSEHOLD ITEMS:");
+        System.out.println("#8 - Tool Combo $1,699.00");
+        System.out.println("#15 - Water Bottle $35.95");
+        System.out.println("#21 - Toothbrush $39.95");
+        System.out.println("#22 - Toothpaste $5.99");
+        System.out.println("#23 - Dog Food $55.98");
+        System.out.println("#24 - Tool Box $32.39");
+        System.out.println("#25 - Mechanical Pencils $13.80");
 
+        System.out.println("------------------------------");
+        System.out.println("GIFT ITEMS:");
+        System.out.println("#2 - Dinosaur Skull $547.80 ");
+        System.out.println("#12 - Binoculars $35.99");
+        System.out.println("#13 - Baseball Cards $39.97");
+        System.out.println("------------------------------");
+    }
 
-        switch (name) {
-            case "0" -> {
-                name = "Ego 56-volt Select Cut";
-            }
-            case "1" -> {
-                name = "iPhone 13 Pro Max 1TB";
-            }
-            case "2" -> {
-                name = "T-Rex Skull";
-            }
-            case "3" -> {
-                name = "Playstation 5";
-            }
-            case "4" -> {
-                name = "Tag Heur Monaco 11";
-            }
-            case "5" -> {
-                name = "Macbook Pro 16-inch";
-            }
-            case "6" -> {
-                name = "Airpods Max";
-            }
-            case "7" -> {
-                name = "Samsung 65-inch 4K Smart TV";
-            }
-            case "8" -> {
-                name = "Milwaukee Brushless Combo";
-            }
-            case "9" -> {
-                name = "iPad Pro 12.9-inch 2TB";
-            }
-            case "10" -> {
-                name = "NexiGo Webcam";
-            }
-            case "11" -> {
-                name = "Blurams";
-            }
-            case "12" -> {
-                name = "Occer 12x25 Compact Binoculars";
-            }
-            case "13" -> {
-                name = "Tops Opening Day Baseball Value Pack";
-            }
-            case "14" -> {
-                name = "Retro Typewriter Keyboard 7 Keys";
-            }
-            case "15" -> {
-                name = "Hydro Flask";
-            }
-            case "16" -> {
-                name = "Auking Mini Projector";
-            }
-            case "17" -> {
-                name = "Nike Tech Jacket";
-            }
-            case "18" -> {
-                name = "Yeezy Boost 350 v2 (bred)";
-            }
-            case "19" -> {
-                name = "Elden Ring";
-            }
-            case "20" -> {
-                name = "Comfort Carry Portable Locker Pack, 42L";
-            }
-            case "21" -> {
-                name = "AquaSonic Black Series";
-            }
-            case "22" -> {
-                name = "Crest Pro-Health";
-            }
-            case "23" -> {
-                name = "Blue Buffalo Life Protection";
-            }
-            case "24" -> {
-                name = "WEWLINE 17-Inch Tool Box";
-            }
-            case "25" -> {
-                name = "BIC Xtra-Smooth Mechanical Pencils";
-            }
-            case "26" -> {
-                name = "3 Pack (AppleMFi Certified)";
-            }
-            case "27" -> {
-                name = "Amazon Basics Small Digital Alarm Clock";
-            }
-            case "28" -> {
-                name = "Tesla Model 3 all Decked out";
-            }
-            case "29" -> {
-                name = "Meepo V3 electric Skateboard";
-            }
-
-        }
+    public int enterItemId() {
+        Scanner name = new Scanner(System.in);
+        int item_id = name.nextInt();
+        return item_id;
     }
 }
